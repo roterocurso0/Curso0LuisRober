e7aac53ef0b3f42e3c888c0d458b735cd7b29dd0
Unknown <luis.giraldo.gonzalez@gmail.com>
2017-12-18 15:50:43 +0100

    Se mejoraron los comentarios

--- Código/Logistic.r
+++ Código/Logistic.r
@@ -1,7 +1,12 @@
-##Se calculad los valores de la ecuación logística
+#================================================================
+#luisggon@math.uc3m.es
+#Luis Giraldo 
+#18 diciembre 2017
+#================================================================
+##Se calculan los valores de la ecuación logística
 ## Se reciben como parámetros la razón de crecimiento r, y el valor de y.
 ##Se devuelve la evaluación de la ecuación logísitcia dy
-##Luis Giraldo 18/12
+#===============================================================
 logistic <- function(r,y)
 {
   return(dy=r*x*(1-x))
